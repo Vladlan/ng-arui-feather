4ffd07639d3602e3eea6d6461693bb6000935899
Vladlan <lanvlad911@gmail.com>
2019-12-27 01:49:00 +0300

    first commit

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,128 @@
+import {NgModule, Directive, Component, ElementRef, EventEmitter, AfterViewInit, Output, OnDestroy, Input} from '@angular/core';
+// import {DomHandler} from 'primeng/dom';
+import {CommonModule} from '@angular/common';
+
+@Directive({
+    selector: '[pButton]'
+})
+export class ButtonDirective implements AfterViewInit, OnDestroy {
+
+    @Input() iconPos: 'left' | 'right' = 'left';
+
+    @Input() cornerStyleClass = 'ui-corner-all';
+
+    public _label: string;
+
+    public _icon: string;
+
+    public initialized: boolean;
+
+    constructor(public el: ElementRef) {}
+
+    ngAfterViewInit() {
+        // DomHandler.addMultipleClasses(this.el.nativeElement, this.getStyleClass());
+        if (this.icon) {
+            const iconElement = document.createElement('span');
+            iconElement.setAttribute('aria-hidden', 'true');
+            const iconPosClass = (this.iconPos === 'right') ? 'ui-button-icon-right' : 'ui-button-icon-left';
+            iconElement.className = iconPosClass  + ' ui-clickable ' + this.icon;
+            this.el.nativeElement.appendChild(iconElement);
+        }
+
+        const labelElement = document.createElement('span');
+        labelElement.className = 'ui-button-text ui-clickable';
+        labelElement.appendChild(document.createTextNode(this.label || 'ui-btn'));
+        this.el.nativeElement.appendChild(labelElement);
+        this.initialized = true;
+    }
+
+    getStyleClass(): string {
+        let styleClass = 'ui-button ui-widget ui-state-default ' + this.cornerStyleClass;
+        if (this.icon) {
+            if (this.label !== null && this.label !== undefined) {
+                if (this.iconPos === 'left') {
+                    styleClass = styleClass + ' ui-button-text-icon-left';
+                } else {
+                    styleClass = styleClass + ' ui-button-text-icon-right';
+                }
+            } else {
+                styleClass = styleClass + ' ui-button-icon-only';
+            }
+        } else {
+            if (this.label) {
+                styleClass = styleClass + ' ui-button-text-only';
+            } else {
+                styleClass = styleClass + ' ui-button-text-empty';
+            }
+        }
+
+        return styleClass;
+    }
+
+    @Input() get label(): string {
+        return this._label;
+    }
+
+    set label(val: string) {
+        this._label = val;
+
+        // if(this.initialized) {
+        //     DomHandler.findSingle(this.el.nativeElement, '.ui-button-text').textContent = this._label;
+
+        //     if(!this.icon) {
+        //         if (this._label) {
+        //             DomHandler.removeClass(this.el.nativeElement, 'ui-button-text-empty');
+        //             DomHandler.addClass(this.el.nativeElement, 'ui-button-text-only');
+        //         }
+        //         else {
+        //             DomHandler.addClass(this.el.nativeElement, 'ui-button-text-empty');
+        //             DomHandler.removeClass(this.el.nativeElement, 'ui-button-text-only');
+        //         }
+        //     }
+        // }
+    }
+
+    @Input() get icon(): string {
+        return this._icon;
+    }
+
+    set icon(val: string) {
+        this._icon = val;
+
+        if (this.initialized) {
+            const iconPosClass = (this.iconPos == 'right') ? 'ui-button-icon-right' : 'ui-button-icon-left';
+            // DomHandler.findSingle(this.el.nativeElement, '.ui-clickable').className =
+            //     iconPosClass + ' ui-clickable ' + this.icon;
+        }
+    }
+
+    ngOnDestroy() {
+        while (this.el.nativeElement.hasChildNodes()) {
+            this.el.nativeElement.removeChild(this.el.nativeElement.lastChild);
+        }
+        this.initialized = false;
+    }
+}
+
+@Component({
+    selector: 'p-button',
+    template: './button.component.html'
+})
+export class Button {
+    @Input() type: string;
+    @Input() iconPos = 'left';
+    @Input() icon: string;
+    @Input() label: string;
+    @Input() disabled: boolean;
+    @Input() style: any;
+    @Input() styleClass: string;
+    @Output() onClick: EventEmitter<any> = new EventEmitter();
+    @Output() onFocus: EventEmitter<any> = new EventEmitter();
+    @Output() onBlur: EventEmitter<any> = new EventEmitter();
+}
+@NgModule({
+    imports: [CommonModule],
+    exports: [ButtonDirective, Button],
+    declarations: [ButtonDirective, Button]
+})
+export class ButtonModule { }
